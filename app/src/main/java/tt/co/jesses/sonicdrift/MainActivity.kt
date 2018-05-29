fa88d3f4ebdc568a8b1ee0add75815ee7823ba7d
Jesse Scott <jessecolinscott@gmail.com>
2018-05-28 22:05:57 -0700

    Refactored to Android X

--- app/src/main/java/tt/co/jesses/sonicdrift/MainActivity.kt
+++ app/src/main/java/tt/co/jesses/sonicdrift/MainActivity.kt
@@ -1,7 +1,7 @@
 package tt.co.jesses.sonicdrift
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
